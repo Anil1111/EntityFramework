ba2f10df18cef6ab6ac87ffcb1af527f242a4382
anilsri.04@Gmail.com <anilsri.04@Gmail.com>
2019-08-28 10:48:38 +0530

    EF

--- ConsoleApp29/Program.cs
+++ ConsoleApp29/Program.cs
@@ -9,6 +9,29 @@ namespace ConsoleApp29
     {
         static void Main(string[] args)
         {
+            SampleDBEntities ObjSample = new SampleDBEntities();
+            List<CustomerTbl> ObjCustomer = ObjSample.CustomerTbls.ToList();
+
+
+
+
+           
+            CustomerTbl cc = ObjCustomer[1];
+
+
+
+            cc.CustomerName = "SWATI";
+            ObjSample.SaveChanges();
+
+            //  ObjSample.Employees.Add(eee);//In Memory
+            ObjSample.SaveChanges();//Data base
+            Console.WriteLine("Record Inserted         ");
+
+           foreach (CustomerTbl ee in ObjCustomer)
+              Console.WriteLine("CUST Id     " + ee.CustomerID  + "CUST Name      " + ee.CustomerName );
+            Console.WriteLine("Happy Coding       ");
+
+            Console.ReadLine();
         }
     }
 }
